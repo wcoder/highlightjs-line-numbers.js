dac4da54526644068c17ccb78b51561cf54c47a0
Evgeniy Pakalo <evgeniy.pakalo@gmail.com>
2015-04-19 20:22:37 +0300

    Fixed plugin

--- src/highlightjs-line-numbers.js
+++ src/highlightjs-line-numbers.js
@@ -1,18 +1,17 @@
 (function (w) {
 	'use strict';
 
-	if (typeof w.hljs == undefined) {
+	if (typeof w.hljs === 'undefined') {
 		console.error('highlight.js not detected!');
 	} else {
 		w.hljs.initLineNumbersOnLoad = initLineNumbersOnLoad;
 		w.hljs.lineNumbersBlock = lineNumbersBlock;
 	}
 
-
 	function initLineNumbersOnLoad () {
 		w.addEventListener('load', function () {
 			try {
-				var blocks = document.getElementsByClassName('hljs');
+				var blocks = document.querySelectorAll('code.hljs');
 
 				for (var i in blocks) {
 					if (blocks.hasOwnProperty(i)) {
@@ -31,15 +30,16 @@
 		var parent = element.parentNode;
 		var lines = parent.outerText.match(/\n/g);
 
-		lines = lines ? lines.length : 0;
+		lines = (lines ? lines.length : 0);// TODO: for IE + 1;
 		if (lines > 1) {
 			var l = '';
 			for (var i = 0; i < lines; i++) {
-				l += (i+1) + '\n';
+				l += (i + 1) + '\n';
 			}
 
 			var linesPanel = document.createElement('code');
-			linesPanel.className = 'hljs-line-numbers';
+			linesPanel.className = 'hljs hljs-line-numbers';
+			linesPanel.style.float = 'left';
 			linesPanel.innerText = l;
 
 			parent.insertBefore(linesPanel, element);
